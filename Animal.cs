97396646715b2711238e21c2e62a64f8b53a0e0c
Ionut Balica <balicaionut@gmail.com>
2020-02-17 19:41:49 +0200

    initial commit
    initial commit

--- Animal.cs
+++ Animal.cs
@@ -8,14 +8,26 @@ namespace FourthAssignmentCSharp
 {
     class Animal
     {
-        public string tipAnimal { get; set; }
-        public string culoare { get; set; }
-        public int varsta { get; set; }
-
+        public string typeAnimal { get; set; }
+        public string color { get; set; }
+        public int age { get; set; }
         public Animal()
         {
-
+            typeAnimal = "";
+            color = "";
+            age = 0;
         }
-
+        public Animal(string type)
+        {
+            this.typeAnimal = type;
+            color = "";
+            age = 0;
+        }
+        public Animal(string type, string color, int age)
+        {
+            this.typeAnimal = type;
+            this.color = color;
+            this.age = age;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,9 +10,53 @@ namespace FourthAssignmentCSharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine();
+            Console.WriteLine("--- Animal exercise -----------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadKey();
+
+            Animal animal1 = new Animal();
+            animal1.typeAnimal = "caine";
+            animal1.color = "negru";
+            animal1.age = 3;
+            Console.WriteLine();
+            Console.WriteLine("Tip: " + animal1.typeAnimal + "\nCuloare: " + animal1.color + "\nVarsta: " + animal1.age);
+
+            Animal animal2 = new Animal("pisica");
+            animal1.color = "alb";
+            animal1.age = 6;
+            Console.WriteLine();
+            Console.WriteLine("Tip: " + animal2.typeAnimal + "\nCuloare: " + animal2.color + "\nVarsta: " + animal2.age);
+
+            Animal animal3 = new Animal("hamster", "roscat", 1);
+            Console.WriteLine();
+            Console.WriteLine("Tip: " + animal3.typeAnimal + "\nCuloare: " + animal3.color + "\nVarsta: " + animal3.age);
+            
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue!");
+
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("--- House exercise -----------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadKey();
+            Console.WriteLine();
 
 
 
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue! ;)");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("--- Bonus :) -----------------------------------");
+            Console.WriteLine();
+                       
+
+
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadKey();
         }
     }
 }
