8fb0085a43e2fe71f77680109ecf9e9736847af6
Ionut Balica <balicaionut@gmail.com>
2020-02-17 22:11:05 +0200

    final commit
    final commit

---
+++ House.cs
@@ -0,0 +1,27 @@
+﻿namespace FourthAssignmentCSharp
+{
+    class House
+    {
+        public string typeHouse { get; set; }
+        public int noFloors { get; set; }
+        public double price { get; set; }
+        public House()
+        {
+            typeHouse = "";
+            noFloors = 0;
+            price = 0.0;
+        }
+        public House(string type)
+        {
+            this.typeHouse = type;
+            noFloors = 0;
+            price = 0.0;
+        }
+        public House(string type, int floors, double price)
+        {
+            this.typeHouse = type;
+            this.noFloors = floors;
+            this.price = price;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,49 +11,76 @@ namespace FourthAssignmentCSharp
         static void Main(string[] args)
         {
             Console.WriteLine();
-            Console.WriteLine("--- Animal exercise -----------------------------------");
+            Console.WriteLine("--- Animal Objects Exercise -----------------------------------");
             Console.WriteLine();
             Console.WriteLine("Press any key to continue!");
             Console.ReadKey();
 
             Animal animal1 = new Animal();
-            animal1.typeAnimal = "caine";
-            animal1.color = "negru";
+            animal1.typeAnimal = "dog";
+            animal1.color = "brwn";
             animal1.age = 3;
             Console.WriteLine();
-            Console.WriteLine("Tip: " + animal1.typeAnimal + "\nCuloare: " + animal1.color + "\nVarsta: " + animal1.age);
+            Console.WriteLine("Type: " + animal1.typeAnimal + "\nColor: " + animal1.color + "\nAge: " + animal1.age);
 
-            Animal animal2 = new Animal("pisica");
-            animal1.color = "alb";
-            animal1.age = 6;
+            Animal animal2 = new Animal("cat");
+            animal2.color = "white";
+            animal2.age = 6;
             Console.WriteLine();
-            Console.WriteLine("Tip: " + animal2.typeAnimal + "\nCuloare: " + animal2.color + "\nVarsta: " + animal2.age);
+            Console.WriteLine("Type: " + animal2.typeAnimal + "\nColor: " + animal2.color + "\nAge: " + animal2.age);
 
-            Animal animal3 = new Animal("hamster", "roscat", 1);
+            Animal animal3 = new Animal("hamster", "red", 1);
             Console.WriteLine();
-            Console.WriteLine("Tip: " + animal3.typeAnimal + "\nCuloare: " + animal3.color + "\nVarsta: " + animal3.age);
+            Console.WriteLine("Type: " + animal3.typeAnimal + "\nColor: " + animal3.color + "\nAge: " + animal3.age);
             
             Console.WriteLine();
             Console.WriteLine("Press any key to continue!");
 
             Console.ReadKey();
             Console.WriteLine();
-            Console.WriteLine("--- House exercise -----------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("--- House Objects Exercise -----------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadKey();
+
+            House house1 = new House();
+            house1.typeHouse = "duplex";
+            house1.noFloors = 2;
+            house1.price = 185000.00;
+            Console.WriteLine();
+            Console.WriteLine("Type: " + house1.typeHouse + "\nFloors: " + house1.noFloors + "\nPrice: " + house1.price);
+
+            House house2 = new House("villa");
+            house2.noFloors = 3;
+            house2.price = 475000.00;
+            Console.WriteLine();
+            Console.WriteLine("Type: " + house2.typeHouse + "\nFloors: " + house2.noFloors + "\nPrice: " + house2.price);
+
+            House house3 = new House("apartment", 1, 65500.00);
+            Console.WriteLine();
+            Console.WriteLine("Type: " + house3.typeHouse + "\nFloors: " + house3.noFloors + "\nPrice: " + house3.price);
+            
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("--- Bonus :) -----------------------------------");
             Console.WriteLine();
             Console.WriteLine("Press any key to continue!");
             Console.ReadKey();
             Console.WriteLine();
 
-
-
+            Console.WriteLine($"The {animal1.color} {animal1.typeAnimal} is {animal1.age} year old");
+            Console.WriteLine($"and it lives in the {house1.noFloors} floors {house1.typeHouse}.");
             Console.WriteLine();
-            Console.WriteLine("Press any key to continue! ;)");
-            Console.ReadKey();
+            Console.WriteLine($"The {animal2.color} {animal2.typeAnimal} is {animal2.age} year old");
+            Console.WriteLine($"and it lives in the {house2.noFloors} floors fancy {house2.typeHouse}.");
             Console.WriteLine();
-            Console.WriteLine("--- Bonus :) -----------------------------------");
+            Console.WriteLine($"But the poor little {animal3.color} {animal3.typeAnimal}");
+            Console.WriteLine($"lives in a humbel {house3.typeHouse}.");
             Console.WriteLine();
-                       
-
 
             Console.WriteLine("Press any key to exit!");
             Console.ReadKey();
